4d5c34b4995e37947301ed7107a9c33a7491b314
runba3011 <runba3011@gmail.com>
2021-08-27 09:26:29 +0900

    Usersテーブルのバリデーションの作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  with_options presence: true do
+    validates :nickname
+    validatae :first_name
+    validates :last_name
+    validates :first_name_reading
+    validates :last_name_reading
+    validates :birthday
+    validates :password,format:{with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]{6,}/}
+  end
 end
