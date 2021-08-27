d04dbd6308bded9c1fa7e4709cb1ce93b2d9f641
runba3011 <runba3011@gmail.com>
2021-08-27 21:33:26 +0900

    Itemテーブルのバリデーションの修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,22 +1,24 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
-  validates :name           , presence: true
-  validates :explain        , presence: true , length:{maximum: 40}
+  validates :name           , presence: true , length:{maximum: 40}
+  validates :explain        , presence: true , length:{maximum: 1000}
   validates :category_id    , presence: true , numericality:{other_than: 0}
   validates :status_id      , presence: true , numericality:{other_than: 0}
   validates :send_cost_id   , presence: true , numericality:{other_than: 0}
   validates :prefecture_id  , presence: true , numericality:{other_than: 0}
   validates :send_limit_id  , presence: true , numericality:{other_than: 0}
-  validates :price          , presence: true
+  validates :price          , presence: true , numericality:{only_integer: true , greater_than_or_equal_to: 300, less_than_or_equal_to:9999999}
   validates :user           , presence: true
-  has_one_attached :image   , presence: true
+
+  has_one_attached :image   
+  validates :image , presence: true
 
   belongs_to :user
   
-  belongs_to :category #
-  belongs_to :status #
-  belongs_to :send_cost#
-  belongs_to :prefecture#
+  belongs_to :category 
+  belongs_to :status 
+  belongs_to :send_cost
+  belongs_to :prefecture
   belongs_to :send_limit 
 
 end
