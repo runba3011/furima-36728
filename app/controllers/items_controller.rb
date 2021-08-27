644df9e06d4e0838271be2d9fb7e2f04c7a38aae
runba3011 <runba3011@gmail.com>
2021-08-27 21:33:00 +0900

    Itemコントローラーcreate,indexアクションの修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,11 +1,25 @@
 class ItemsController < ApplicationController
   def index
+    @items = Item.all
   end
 
   def create
+    @item = Item.new(item_params)
+    if @item.valid?
+      @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   def new 
     @item = Item.new
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:category_id , :image , :name , :explain , :category_id, :status_id , :send_cost_id , :prefecture_id , :send_limit_id , :price).merge(user_id: current_user.id )
   end
 end
