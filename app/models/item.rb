f3483c9c77e007caac635ba2158ce8e2016eb241
runba3011 <runba3011@gmail.com>
2021-08-28 11:31:21 +0900

    with_optionsを用いたバリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,20 +1,25 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
-  validates :name           , presence: true , length:{maximum: 40}
-  validates :explain        , presence: true , length:{maximum: 1000}
-  validates :category_id    , presence: true , numericality:{other_than: 0, message: "can't be blank"}
-  validates :status_id      , presence: true , numericality:{other_than: 0, message: "can't be blank"}
-  validates :send_cost_id   , presence: true , numericality:{other_than: 0, message: "can't be blank"}
-  validates :prefecture_id  , presence: true , numericality:{other_than: 0, message: "can't be blank"}
-  validates :send_limit_id  , presence: true , numericality:{other_than: 0, message: "can't be blank"}
-  validates :price          , presence: true , numericality:{only_integer: true , greater_than_or_equal_to: 300, less_than_or_equal_to:9999999}
-  validates :user           , presence: true
-
   has_one_attached :image   
-  validates :image , presence: true
+
+  with_options presence: true do
+    
+    validates :name    , length:{maximum: 40}
+    validates :explain , length:{maximum: 1000}
+    validates :price   , numericality:{only_integer: true , greater_than_or_equal_to: 300, less_than_or_equal_to:9999999}
+    validates :user           
+    validates :image          
+
+    with_options numericality:{other_than: 0 , message: "can't be blank"} do
+      validates :category_id    
+      validates :status_id      
+      validates :send_cost_id   
+      validates :prefecture_id  
+      validates :send_limit_id  
+    end
+  end
 
   belongs_to :user
-  
   belongs_to :category 
   belongs_to :status 
   belongs_to :send_cost
