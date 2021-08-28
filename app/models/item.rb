70fcf0d46d5be5acd955cc03772a040c4dc2251d
runba3011 <runba3011@gmail.com>
2021-08-28 09:21:52 +0900

    選択ボックスを選択していない時のエラーメッセージの修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,11 +2,11 @@ class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   validates :name           , presence: true , length:{maximum: 40}
   validates :explain        , presence: true , length:{maximum: 1000}
-  validates :category_id    , presence: true , numericality:{other_than: 0}
-  validates :status_id      , presence: true , numericality:{other_than: 0}
-  validates :send_cost_id   , presence: true , numericality:{other_than: 0}
-  validates :prefecture_id  , presence: true , numericality:{other_than: 0}
-  validates :send_limit_id  , presence: true , numericality:{other_than: 0}
+  validates :category_id    , presence: true , numericality:{other_than: 0, message: "can't be blank"}
+  validates :status_id      , presence: true , numericality:{other_than: 0, message: "can't be blank"}
+  validates :send_cost_id   , presence: true , numericality:{other_than: 0, message: "can't be blank"}
+  validates :prefecture_id  , presence: true , numericality:{other_than: 0, message: "can't be blank"}
+  validates :send_limit_id  , presence: true , numericality:{other_than: 0, message: "can't be blank"}
   validates :price          , presence: true , numericality:{only_integer: true , greater_than_or_equal_to: 300, less_than_or_equal_to:9999999}
   validates :user           , presence: true
 
