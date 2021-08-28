c196918818fa7821b98908d224e27b591c0c2533
runba3011 <runba3011@gmail.com>
2021-08-28 09:11:15 +0900

    販売手数料を算出するスクリプトの作成

---
+++ app/javascript/calc_benefit.js
@@ -0,0 +1,16 @@
+const calc = () =>{
+  console.log("計算用のスクリプトです");
+  const commission_rate = 0.1 //販売手数料の割合
+
+  const root_price = document.getElementById('item-price');
+  const commission = document.getElementById('add-tax-price'); //販売手数料、root_priceの１０％
+  const profit = document.getElementById('profit'); //root_priceから販売手数料を引いた額
+
+  root_price.addEventListener('input' , ()=>{
+    commission.innerHTML = root_price.value * commission_rate;
+    profit.innerHTML = root_price.value - commission.innerHTML;
+  })
+}
+
+
+window.addEventListener('load' , calc);
