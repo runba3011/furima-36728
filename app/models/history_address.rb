1f1d1b21bcddcf16bdc67fa0c3a84e36036723ee
runba3011 <runba3011@gmail.com>
2021-08-30 18:33:18 +0900

    historyとaddressの両方のバリデーションを同時に行うモデルの作成

---
+++ app/models/history_address.rb
@@ -0,0 +1,23 @@
+class HistoryAddress
+  include ActiveModel::Model
+  attr_accessor :post_number , :prefecture_id , :city ,:building_number , :building_name ,:phone_number ,  :user_id , :item_id
+
+  with_options presence: true do
+    validates :user
+    validates :item
+    validates :address
+    validates :post_number , format: {with: /\A\d{3}[-]\d{4}\z/}
+    validates :prefecture_id , numericality: {only_integer: true , greater_than: 0 , message: "can't be blank"}
+    validates :city
+    validates :building_number
+    validates :phone_number ,length: {maximum: 11 , minimum: 10}  , format: {with: /\A[0-9]+\z/}
+    validates :history 
+  end
+
+  def save
+    history = History.new(user_id: user_id , item_id: item_id)
+    Address.create(post_number: post_number , prefecture_id: prefecture_id , city: city , building_number: building_number , phone_number: phone_number , building_name: building_name , history_id: history.id) 
+    bunding.pry
+    
+  end
+end
