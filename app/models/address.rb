326676eed9e5ea34dfb8044fbb63278f1d4402ff
runba3011 <runba3011@gmail.com>
2021-08-30 18:33:44 +0900

    addressテーブルとhistoryテーブルのバリデーションをコメントアウト

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,11 +1,11 @@
 class Address < ApplicationRecord
-  with_options presence: true do
-    validates :post_number , format: {with: /\A\d{3}[-]\d{4}\z/}
-    validates :prefecture_id , numericality: {only_integer: true , greater_than: 0 , message: "can't be blank"}
-    validates :city
-    validates :building_number
-    validates :phone_number ,length: {maximum: 11 , minimum: 10}  , format: {with: /\A[0-9]+\z/}
-    validates :history 
-  end
+  # with_options presence: true do
+  #   validates :post_number , format: {with: /\A\d{3}[-]\d{4}\z/}
+  #   validates :prefecture_id , numericality: {only_integer: true , greater_than: 0 , message: "can't be blank"}
+  #   validates :city
+  #   validates :building_number
+  #   validates :phone_number ,length: {maximum: 11 , minimum: 10}  , format: {with: /\A[0-9]+\z/}
+  #   validates :history 
+  # end
   belongs_to :history
 end
--- app/models/history.rb
+++ app/models/history.rb
@@ -1,9 +1,9 @@
 class History < ApplicationRecord
-  with_options presence: true do
-    validates :user
-    validates :item
-    validates :address
-  end
+  # with_options presence: true do
+  #   validates :user
+  #   validates :item
+  #   validates :address
+  # end
   
   belongs_to :user
   belongs_to :item
