39ede433af37910b87428cac80ee1e87599d9f59
runba3011 <runba3011@gmail.com>
2021-08-30 12:49:57 +0900

    コメントアウトの削除

--- app/javascript/calc_benefit.js
+++ app/javascript/calc_benefit.js
@@ -1,22 +1,19 @@
 const calc = () =>{
-  const commission_rate = 0.1 //販売手数料の割合
+  const commission_rate = 0.1 
 
   const root_price = document.getElementById('item-price');
-  const commission = document.getElementById('add-tax-price'); //販売手数料、root_priceの１０％
-  const profit = document.getElementById('profit'); //root_priceから販売手数料を引いた額
+  const commission = document.getElementById('add-tax-price'); 
+  const profit = document.getElementById('profit'); 
 
-  //販売手数料を計算するタイミングで呼び出すもの
   const calculate =  function(){
     commission.innerHTML = Math.floor(root_price.value * commission_rate);
     profit.innerHTML = root_price.value - commission.innerHTML;
   }
 
-  //商品情報編集時、ロード時点で販売手数料などを計算するためのもの
   if(root_price.value != ''){
     calculate();
   };
 
-  //入力時に販売手数料を計算する
   root_price.addEventListener('input' , calculate);
 }
 
