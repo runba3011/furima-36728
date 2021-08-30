b6c93651d26207e799535ad40eeeafa7e934fe20
runba3011 <runba3011@gmail.com>
2021-08-30 21:25:35 +0900

    history_address用のFactoryBotの生成

---
+++ spec/factories/history_address.rb
@@ -0,0 +1,9 @@
+FactoryBot.define do
+  factory :history_address do
+    post_number {'444-4444'}
+    prefecture_id {25}
+    city {"名古屋市"}
+    building_number {'1-32-1'}
+    phone_number {"08019191919"}
+  end
+end
