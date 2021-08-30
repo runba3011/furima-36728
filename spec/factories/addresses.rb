7fa7117da0bdc03676c2841e2966effd3d4a6a43
runba3011 <runba3011@gmail.com>
2021-08-30 17:03:30 +0900

    addresesテーブル用FactoryBotの作成

--- spec/factories/addresses.rb
+++ spec/factories/addresses.rb
@@ -1,5 +1,10 @@
 FactoryBot.define do
   factory :address do
-    
+    post_number {444-4444}
+    prefecture_id {25}
+    city {"名古屋市"}
+    building_number {'1-32-1'}
+    phone_number {"08019191919"}
+    association :history
   end
 end
