a23c509b3df875aeb10a5b84db5d693fa801988b
runba3011 <runba3011@gmail.com>
2021-08-30 17:03:57 +0900

    historiesテーブル用テストコードへの追記

--- spec/models/history_spec.rb
+++ spec/models/history_spec.rb
@@ -3,11 +3,13 @@ require 'rails_helper'
 RSpec.describe History, type: :model do
   before do
     @history = FactoryBot.build(:history)
+    @address = FactoryBot.build(:address)
+    @history.address = @address
   end
 
   describe '購入履歴の保存ができるとき' do
     it 'user,itemに紐づいており、かつaddressテーブルの情報を持っているとき' do
-      binding.pry
+      expect
     end
   end
 
