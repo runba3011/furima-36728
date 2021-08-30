0a9f545f32904ed853204d8b4336d227d0b1fe39
runba3011 <runba3011@gmail.com>
2021-08-30 21:26:15 +0900

    history_addressモデルのバリデーションの設定

--- app/models/history_address.rb
+++ app/models/history_address.rb
@@ -3,21 +3,19 @@ class HistoryAddress
   attr_accessor :post_number , :prefecture_id , :city ,:building_number , :building_name ,:phone_number ,  :user_id , :item_id
 
   with_options presence: true do
-    validates :user
-    validates :item
-    validates :address
+    validates :user_id
+    validates :item_id
     validates :post_number , format: {with: /\A\d{3}[-]\d{4}\z/}
     validates :prefecture_id , numericality: {only_integer: true , greater_than: 0 , message: "can't be blank"}
     validates :city
     validates :building_number
     validates :phone_number ,length: {maximum: 11 , minimum: 10}  , format: {with: /\A[0-9]+\z/}
-    validates :history 
   end
 
   def save
-    history = History.new(user_id: user_id , item_id: item_id)
-    Address.create(post_number: post_number , prefecture_id: prefecture_id , city: city , building_number: building_number , phone_number: phone_number , building_name: building_name , history_id: history.id) 
-    bunding.pry
+    @history = History.create(user_id: user_id , item_id: item_id)
+    @address =  Address.create(post_number: post_number , prefecture_id: prefecture_id , city: city , building_number: building_number , phone_number: phone_number , building_name: building_name , history_id: @history.id) 
+    binding.pry
     
   end
 end
