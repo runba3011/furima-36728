ce154687158c5f277ea40048dc7f001650f0ab51
runba3011 <runba3011@gmail.com>
2021-08-30 20:45:29 +0900

    ルーティングの変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "items#index"
-  resources :items
+  resources :items do
+    resources :order , only: [:create]
+  end
   resources :order , only: [:show]
 end
