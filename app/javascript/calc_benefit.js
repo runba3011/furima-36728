aface77358aabc2cb1dd3a51b3437263382a7541
runba3011 <runba3011@gmail.com>
2021-08-30 11:17:59 +0900

    ページ読み込み時、既に金額が入っていた時に販売手数料を計算する処理の作成

--- app/javascript/calc_benefit.js
+++ app/javascript/calc_benefit.js
@@ -5,11 +5,21 @@ const calc = () =>{
   const commission = document.getElementById('add-tax-price'); //販売手数料、root_priceの１０％
   const profit = document.getElementById('profit'); //root_priceから販売手数料を引いた額
 
-  root_price.addEventListener('input' , ()=>{
+  //販売手数料を計算するタイミングで呼び出すもの
+  const calculate =  function(){
     commission.innerHTML = Math.floor(root_price.value * commission_rate);
     profit.innerHTML = root_price.value - commission.innerHTML;
-  })
+  }
+
+  //商品情報編集時、ロード時点で販売手数料などを計算するためのもの
+  if(root_price.value != ''){
+    calculate();
+  };
+
+  //入力時に販売手数料を計算する
+  root_price.addEventListener('input' , calculate);
 }
+
 
 
 window.addEventListener('load' , calc);
