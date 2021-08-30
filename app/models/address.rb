10fbb72335cef8acf74f8695ff22c7dee0a1760e
runba3011 <runba3011@gmail.com>
2021-08-30 18:01:03 +0900

    Addressesテーブルのバリデーションの修正

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,10 +1,10 @@
 class Address < ApplicationRecord
   with_options presence: true do
-    validates :post_number
-    validates :prefecture_id
+    validates :post_number , format: {with: /\A\d{3}[-]\d{4}\z/}
+    validates :prefecture_id , numericality: {only_integer: true , greater_than: 0 , message: "can't be blank"}
     validates :city
     validates :building_number
-    validates :phone_number
+    validates :phone_number ,length: {maximum: 11 , minimum: 10}  , format: {with: /\A[0-9]+\z/}
     validates :history 
   end
   belongs_to :history
