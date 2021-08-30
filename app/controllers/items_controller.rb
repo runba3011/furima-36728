c0ff38cb01057c69e7d2b4d2e7cbb1b4c8b3c4e6
runba3011 <runba3011@gmail.com>
2021-08-30 14:19:21 +0900

    削除処理へのアクセス制限のさらなる追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show, :destroy]
   before_action :find_item , only: [:show,:edit,:update , :destroy]
-  before_action :confirm_same_user, only: [:edit , :update]
+  before_action :confirm_same_user, only: [:edit , :update, :destroy]
 
   def index
     @items = Item.all.order('created_at DESC')
