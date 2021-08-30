63af9d80651b002d18b79df89f1bd4a25d3244dd
runba3011 <runba3011@gmail.com>
2021-08-30 15:46:42 +0900

    historiesテーブルのテストコードの作成

--- spec/models/history_spec.rb
+++ spec/models/history_spec.rb
@@ -1,4 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe History, type: :model do
+  before do
+    @history = FactoryBot.build(:history)
+  end
+
+  describe '購入履歴の保存ができるとき' do
+    it 'user,itemに紐づいており、かつaddressテーブルの情報を持っているとき' do
+      binding.pry
+    end
+  end
+
+  describe '購入履歴の保存ができないとき' do
+  end
 end
