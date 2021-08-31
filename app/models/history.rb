aa1ee0534fe0807b9208dda00fbf9212739af096
runba3011 <runba3011@gmail.com>
2021-08-31 14:36:02 +0900

    テストコードで確認するバリデーションのコメントアウトの解除

--- app/models/history.rb
+++ app/models/history.rb
@@ -1,13 +1,11 @@
 class History < ApplicationRecord
-  # history_address.rbで保存するようにする前についていたバリデーションになります。
-  # テストコードを使用するときのみ一時的にコメントアウトをはずすようにしてありますので残しておくようよろしくお願いします。
-  # with_options presence: true do
-  #   validates :user
-  #   validates :item
-  #   validates :address
-  # end
+  with_options presence: true do
+    validates :user
+    validates :item
+    validates :address
+  end
   
   belongs_to :user
   belongs_to :item
-  has_one :address
+  has_one :address , dependent: :destroy
 end
