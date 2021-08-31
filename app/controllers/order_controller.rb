cd1ffd4c6bf0ee781619af06c8caca91b5b55a7c
runba3011 <runba3011@gmail.com>
2021-08-31 09:22:42 +0900

    購入機能へのアクセス制限

--- app/controllers/order_controller.rb
+++ app/controllers/order_controller.rb
@@ -1,4 +1,7 @@
 class OrderController < ApplicationController
+  before_action :find_item
+  before_action :buy_limit
+
   def show
     @item = Item.find(params[:id])
     @history_address = HistoryAddress.new
@@ -10,7 +13,6 @@ class OrderController < ApplicationController
       @history_address.save
       redirect_to root_path
     else
-      @item = Item.find(params[:item_id])
       render :show
     end
   end
@@ -20,4 +22,34 @@ class OrderController < ApplicationController
     params.require(:history_address).permit(:post_number , :prefecture_id , :city ,:building_number , :building_name ,:phone_number).merge(user_id: current_user.id , item_id: params[:item_id])
   end
 
+
+  # 同じ処理を使用する頻度が高いものをまとめたもの、単品だけで呼ぶことはまずない↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓
+  # 誤って単品だけで呼ぶ処理を作成したときにわかりやすくするためコメントアウトは残しておいてください
+
+  # paramから該当するItemを探すための処理
+  # なんの処理かわかるようにするためにコメントアウトは残しておいてください
+  def find_item
+    @item = Item.find(params[:id])
+  end
+
+  # itemの出品者と、現在ログインしているユーザーが同じであればtrueを返す処理
+  # なんの処理かわかるようにするためにコメントアウトは残しておいてください
+  def only_confirm_same_user
+    @item.user.id == current_user.id
+  end
+
+  # Itemが売れているならtrueを返す処理
+  # なんの処理かわかるようにするためにコメントアウトは残しておいてください
+  def only_confirm_item_sold
+    @item.history != nil
+  end
+
+  # ↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑
+
+  def buy_limit
+    if only_confirm_same_user || only_confirm_item_sold
+      redirect_to root_path
+    end
+  end
+  
 end
